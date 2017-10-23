72c9d70a22470b962d434dc639d7b45d61a4c015
Evgeny Matveychuk <matveychuk.ev@yandex.ru>
2017-10-24 01:07:12 +0300

    Translates corrected in modules\admin\models\view - 'Change' button.

--- modules/admin/views/litter/view.php
+++ modules/admin/views/litter/view.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('forms', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('forms', 'Change'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('forms', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
